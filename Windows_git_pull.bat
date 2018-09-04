5c918435e8a1fcdd8d34c6e073a0ad72de5bcd96
andreasrmr <andreasrmr@hotmail.com>
2018-09-04 17:32:58 +0200

    .

---
+++ Windows_git_pull.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+echo "------------DB-------------"
+cd db
+git pull
+echo "------------java-------------"
+cd ../java
+git pull
+echo "------------js-------------"
+cd ../js
+git pull
+echo "------------scripts-------------"
+cd ../scripts
+git pull
+echo "------------visualparagdigm-------------"
+cd ../visualparagdigm
+git pull
+pause
---
+++ Windows_git_status.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+echo "------------DB-------------"
+cd db
+git status
+echo "------------java-------------"
+cd ../java
+git status
+echo "------------js-------------"
+cd ../js
+git status
+echo "------------scripts-------------"
+cd ../scripts
+git status
+echo "------------visualparagdigm-------------"
+cd ../visualparagdigm
+git status
+pause
