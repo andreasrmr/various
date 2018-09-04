d23fad2ca8b051bc8ad2eb77dd4045b97671aa93
andreasrmr <andreasrmr@hotmail.com>
2018-09-04 17:09:36 +0200

    .

---
+++ Win_git_pull.bat
@@ -0,0 +1,11 @@
+cd db
+git pull
+cd ../java
+git pull
+cd ../js
+git pull
+cd ../linuxting
+git pull
+cd ../visualparagdigm
+git pull
+pause
---
+++ Win_git_status.bat
@@ -0,0 +1,11 @@
+cd db
+git status
+cd ../java
+git status
+cd ../js
+git status
+cd ../scripts
+git status
+cd ../visualparagdigm
+git status
+pause
