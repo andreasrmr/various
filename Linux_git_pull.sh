12aea15fdf44eac590d43f3cce8742712d31c312
Andreas Rømer <andreasrmr@hotmail.com>
2018-09-16 12:51:25 +0200

    .

--- Linux_git_pull.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#init
-for dir in ~/repositories/*; do (cd "$dir" && echo "----$dir---- " && git pull); done
-read -p "Press enter"
--- Linux_git_status.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#init
-for dir in ~/repositories/*; do (cd "$dir" && echo "----$dir---- " && git status); done
-read -p "Press enter"
