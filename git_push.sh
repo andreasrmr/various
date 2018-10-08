6daccd65bfeea2950cf27d7df213911d3c3d88fc
andreasrmr <andreasrmr@hotmail.com>
2018-10-08 13:13:41 +0200

    .

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #init
-for dir in ../*; do (cd "$dir" && echo $PWD && git add . && git commit -m "." && git push ); done
+for dir in ../*; do (cd "$dir" && printf "$PWD" && git add . && git commit -m "." && git push ); done
 read -p "Press enter"
