f5dc273fd34e3eb1e31a05caa9a4d86ea314a0bd
andreasrmr <andreasrmr@hotmail.com>
2018-12-10 13:54:24 +0100

    .

--- dockerMySQL.sh
+++
@@ -1,9 +0,0 @@
-#pull image
-docker run --name=mysql01 -d mysql/mysql-server:latest
-#start dock
-docker run --name=mysql01 -d mysql/mysql-server:latest
-docker logs mysql01
-echo 'check logs for root password'
-docker exec -it mysql01 mysql -uroot -p
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'ChangeTh!s';
-
--- dockerWebserver.sh
+++
@@ -1,3 +0,0 @@
-docker run -dit --name my-apache-app -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
-docker ps -a
-
