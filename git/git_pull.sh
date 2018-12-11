0622c5e9b30ba69d58456d4880d46abd2d0715f8
andreasrmr <andreasrmr@hotmail.com>
2018-12-11 22:02:54 +0100

    .

--- git/git_pull.sh
+++ git/git_pull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #init
-for dir in ../*; do (cd "$dir" && echo $PWD && git pull); done
+for dir in ../../*; do (cd "$dir" && echo $PWD && git pull); done
 read -p "Press enter"
--- git/git_status.sh
+++ git/git_status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #init
-for dir in ../*; do (cd "$dir" && printf "\n #### $dir ####  \n$PWD \n"  && git status); done
+for dir in ../../*; do (cd "$dir" && printf "\n #### $dir ####  \n$PWD \n"  && git status); done
 read -p "Press enter"
